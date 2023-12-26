789130ef23d7178d7a335ddbf59ea69ec5a60a61
Asef Guluzade <asefdevs@gmail.com>
2023-12-27 01:31:41 +0300

    Add room view to handle chat rooms, add other configurations to project

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -74,6 +74,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chatApp.wsgi.application'
 ASGI_APPLICATION = "chatApp.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 
 # Database
--- chatApp/urls.py
+++ chatApp/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(chat_urls)),
+    path('chat/', include(chat_urls)),
 ]
