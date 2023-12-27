7ecbbe99944c8c837a4c0ba6a4c8645c74ba6ee6
Asef Guluzade <asefdevs@gmail.com>
2023-12-27 14:59:08 +0300

    Remove unnecessary code and update chat functionality

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,8 +8,6 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
-        print('hiiiiiiiiiiiiiiiiiiiiiiiiiii')
-
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
@@ -27,7 +25,6 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat.message", "message": message}
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,10 +1,43 @@
 const roomName = JSON.parse(document.getElementById('room-name').textContent);
+const conversation = document.getElementById('conversation');
 const chatSocket = new WebSocket('ws://'+ window.location.host+ '/ws/chat/'+ roomName+ '/');
+const sendButton = document.querySelector("#send");
+const commentInputDom = document.querySelector("#comment");
 
 chatSocket.onmessage = function(e){
     const data = JSON.parse(e.data);
+    var message = `
+                <div class="row message-body">
+                <div class="col-sm-12 message-main-sender">
+                <div class="sender">
+                    <div class="message-text">
+                    ${data['message']}
+                    </div>
+                    <span class="message-time pull-right">
+                    Sun
+                    </span>
+                </div>
+                </div>
+            </div> 
+        `
+    conversation.innerHTML += message
+}
 
-    console.log(data.messagge);
-    // const message = data['message'];
-    // document.querySelector('#chat-log').value += (message + '\n');
+chatSocket.onclose = function(e){
+    console.error('Chat socket closed unexpectedly');
+}
+
+commentInputDom.focus();
+commentInputDom.onkeyup = function(e){
+    if(e.keyCode === 13){
+        sendButton.click();
+    }
+}
+
+sendButton.onclick = function(e){
+    const message = commentInputDom.value;
+    chatSocket.send(JSON.stringify({
+        'message': message
+    }));
+    commentInputDom.value = '';
 }
